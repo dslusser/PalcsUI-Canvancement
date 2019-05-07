7db6a324163534962ac97fc5bdfe6dd70fc7fba1
Dan Slusser <dslusser@gmail.com>
2019-05-07 14:10:20 -0400

    Version bump and gitcdn.xyz link update.

--- install/palcs-ui.user.js
+++ install/palcs-ui.user.js
@@ -6,7 +6,7 @@
 // @include     https://*.instructure.com/courses/*/quizzes/*/history?*
 // @include     https://*.instructure.com/*
 // @noframes
-// @version     1.02
+// @version     1.03
 // @grant       none
 // ==/UserScript==
 (function() {
@@ -23,11 +23,13 @@
   };
 
   //Gitcdn master palcs-ui-engine.js file
+  //Gitcdn.link url deprecated? Now using gitcdn.xyz
   //https://gitcdn.link/repo/dslusser/PalcsUI-Canvancement/master/src/palcs-ui-engine.js
+  //https://gitcdn.xyz/repo/dslusser/PalcsUI-Canvancement/master/src/palcs-ui-engine.js
   //Localhost file
   //http://localhost:8080/palcs-ui-engine.js
   $.ajax({
-    'url' : 'https://gitcdn.link/repo/dslusser/PalcsUI-Canvancement/master/src/palcs-ui-engine.js',
+    'url' : 'https://gitcdn.xyz/repo/dslusser/PalcsUI-Canvancement/master/src/palcs-ui-engine.js',
     'dataType' : 'script',
     'cache' : true,
     'success' : function() {
