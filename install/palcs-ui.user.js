db4c51fd507f372606ff177576c29da356be572b
Dan Slusser <dslusser@gmail.com>
2019-10-30 09:25:21 -0400

    Removed reliance on jQuery, version bump

--- install/palcs-ui.user.js
+++ install/palcs-ui.user.js
@@ -6,35 +6,38 @@
 // @include     https://*.instructure.com/courses/*/quizzes/*/history?*
 // @include     https://*.instructure.com/*
 // @noframes
-// @version     1.03
+// @version     1.04
 // @grant       none
 // ==/UserScript==
-(function() {
+(function () {
   'use strict';
 
   var config = {
     // PalcsUI enhancements may be true or false
-    'addGradePercentage' : true,
-    'addCustomCSS' : true,
-    'boxResizerCSS' : true,
-    'gradebookTooltipCSS' : true,
-    'keyframesHolderCSS' : true,
-    'addMsisNavigation' : true
+    'addGradePercentage': true,
+    'addCustomCSS': true,
+    'boxResizerCSS': true,
+    'gradebookTooltipCSS': true,
+    'keyframesHolderCSS': true,
+    'addMsisNavigation': true
   };
 
   //Gitcdn master palcs-ui-engine.js file
-  //Gitcdn.link url deprecated? Now using gitcdn.xyz
+  //Gitcdn.link url deprecated. Now using gitcdn.xyz
   //https://gitcdn.link/repo/dslusser/PalcsUI-Canvancement/master/src/palcs-ui-engine.js
   //https://gitcdn.xyz/repo/dslusser/PalcsUI-Canvancement/master/src/palcs-ui-engine.js
   //Localhost file
   //http://localhost:8080/palcs-ui-engine.js
-  $.ajax({
-    'url' : 'https://gitcdn.xyz/repo/dslusser/PalcsUI-Canvancement/master/src/palcs-ui-engine.js',
-    'dataType' : 'script',
-    'cache' : true,
-    'success' : function() {
+
+  if (typeof PalcsUI !== 'function') {
+    const script = document.createElement('script');
+    script.src = 'https://gitcdn.xyz/repo/dslusser/PalcsUI-Canvancement/master/src/palcs-ui-engine.js';
+    script.onload = function () {
       PalcsUI(config);
-    }
-  });
+    };
+    document.head.appendChild(script);
+  } else {
+    PalcsUI(config);
+  }
 
 })();
