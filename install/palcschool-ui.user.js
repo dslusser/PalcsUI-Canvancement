9d4a29222d327e8f53fbc04b042f1664af3cf715
Dan Slusser <DSlusser@gmail.com>
2022-01-24 11:53:08 -0500

    Added IEPWriter nav, dashboardColorsCSS

--- install/palcschool-ui.user.js
+++ install/palcschool-ui.user.js
@@ -5,7 +5,7 @@
 // @include     https://*.palcschool.org/*
 // @include     https://*palcschool.org/*
 // @noframes
-// @version     1.04
+// @version     1.05
 // @grant       none
 // ==/UserScript==
 (function () {
@@ -20,11 +20,15 @@
   // the Palcschool teacher dashboard. Turn it on by setting the
   // value to true. Turn it off by setting it to false.
 
+  // dashboardColorsCSS somewhat fixes the odd color choices on
+  // the Palcschool teacher dashboard. Turn it on by setting the
+  // value to true. Turn it off by setting it to false.
+
   // canvasBulkMessageCSS somewhat fixes the scrolling issues on
   // the Palcschool Canvas Bulk Message page. Turn it on by setting the
   // value to true. Turn it off by setting it to false.
 
-  // addPwrSchoolNavigation adds a PowerSchool navigation item to
+  // addIEPWriterNavigation adds a IEPWriter navigation item to
   // the side navigation menu. Turn it on by setting the
   // value to true. Turn it off by setting it to false.
 
@@ -35,8 +39,9 @@
     // PalcschoolUI enhancements may be true or false
     'removeNewTabs': true,
     'dashboardWidthCSS': true,
+    'dashboardColorsCSS': true,
     'canvasBulkMessageCSS': true,
-    'addPwrSchoolNavigation': true,
+    'addIEPWriterNavigation': true,
     'addByStudentRcLink': true
   };
 
@@ -60,9 +65,10 @@
               'removeNewTabs' : true,
               'addCustomCSS' : true,
               'dashboardWidthCSS' : true,
+              'dashboardColorsCSS': true,
               'canvasBulkMessageCSS': true,
               'keyframesHolderCSS' : true,
-              'addPwrSchoolNavigation' : true,
+              'addIEPWriterNavigation' : true,
               'addByStudentRcLink' : true
           };
       };
@@ -115,35 +121,35 @@
 
       head.appendChild(style);
 
-      //Globals for Speed Grader and pwrSchoolNav functions
+      //Globals for Speed Grader and iepWriterNav functions
       //var getURLArray = document.URL.split(/\?(.+)?/)[0];
       //var parseURL = getURLArray.split('/');
       //var speed_grader = parseURL[6];
       var header = document.getElementsByTagName('header')[0];
 
-      function pwrSchoolNav() {
+      function iepWriterNav() {
 
           if (header !== null && header !== undefined){
               var menu = document.getElementsByClassName('main-nav')[0].firstElementChild;
 
               //var menu = menu.firstElementChild;
-              //console.log('PowerSchool navigation added');
+              //console.log('IEPWriter navigation added');
 
-              var menuPwrSchoolItem = document.createElement("li");
-              menuPwrSchoolItem.className = "menu-item ic-app-header__menu-list-item";
+              var menuIepWriterItem = document.createElement("li");
+              menuIepWriterItem.className = "menu-item ic-app-header__menu-list-item";
               //declared animation fadeIn keyframes in keyframesHolderCSSCode variable
-              menuPwrSchoolItem.style.animation = "fadeIn 150ms ease-in";
+              menuIepWriterItem.style.animation = "fadeIn 150ms ease-in";
 
-              var PwrSchoolLink = '\n  <a id=\"global_nav_pwrSchool_link\" href=\"https:\/\/palcs.powerschool.com\/admin\/home.html\" target=\"_blank\" class=\"ic-app-header__menu-list-link\">\n    <div class=\"menu-item-icon-container\" aria-hidden=\"true\">\n<svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\" width=\"32\" height=\"32\" viewBox=\"0 0 32 32\"><title>icon-powerSchool</title><path fill=\"#ffffff\" d=\"M28.8,0H3.2A3.11,3.11,0,0,0,0,3V29a3.11,3.11,0,0,0,3.2,3H28.8A3.11,3.11,0,0,0,32,29V3A3.11,3.11,0,0,0,28.8,0ZM27.73,28H4.27V4H27.73ZM8.53,18H23.47v2H8.53Zm0,4H23.47v2H8.53ZM10.67,9a3.11,3.11,0,0,1,3.2-3,3.11,3.11,0,0,1,3.2,3,3.11,3.11,0,0,1-3.2,3A3.11,3.11,0,0,1,10.67,9ZM16,12H11.73c-1.76,0-3.2.9-3.2,2v2H19.2V14C19.2,12.9,17.76,12,16,12Z\"></path></svg>\n<span class=\"menu-item__badge\" style=\"display: none\">0</span>\n    </div>\n    <div class=\"menu-item__text\">\n      PowerSchool\n</div>\n  </a>\n';
+              var IepWriterLink = '\n  <a id=\"global_nav_iepWriter_link\" href=\"https:\/\/www.iepwriter.com\/pa\/\" target=\"_blank\" class=\"ic-app-header__menu-list-link\">\n    <div class=\"menu-item-icon-container\" aria-hidden=\"true\">\n<svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\" width=\"32\" height=\"32\" viewBox=\"0 0 32 32\"><title>icon-iepWriter</title><path fill=\"#ffffff\" d=\"M28.8,0H3.2A3.11,3.11,0,0,0,0,3V29a3.11,3.11,0,0,0,3.2,3H28.8A3.11,3.11,0,0,0,32,29V3A3.11,3.11,0,0,0,28.8,0ZM27.73,28H4.27V4H27.73ZM8.53,18H23.47v2H8.53Zm0,4H23.47v2H8.53ZM10.67,9a3.11,3.11,0,0,1,3.2-3,3.11,3.11,0,0,1,3.2,3,3.11,3.11,0,0,1-3.2,3A3.11,3.11,0,0,1,10.67,9ZM16,12H11.73c-1.76,0-3.2.9-3.2,2v2H19.2V14C19.2,12.9,17.76,12,16,12Z\"></path></svg>\n<span class=\"menu-item__badge\" style=\"display: none\">0</span>\n    </div>\n    <div class=\"menu-item__text\">\n      IEPWriter\n</div>\n  </a>\n';
 
 
 
-              menuPwrSchoolItem.innerHTML = PwrSchoolLink;
-              menu.appendChild(menuPwrSchoolItem);
+              menuIepWriterItem.innerHTML = IepWriterLink;
+              menu.appendChild(menuIepWriterItem);
           }
       }
 
-    // POWERSCHOOL NAV @ https://palcs.powerschool.com/admin/home.html
+    // IEPWRITER NAV @ https://www.iepwriter.com/pa/
 
 
 
@@ -158,6 +164,43 @@
       var dashboardWidthCSSCode = `
       .chart-container {
         width: 100% !important;
+      }`;
+
+      var dashboardColorsCSSCode = `
+      .section.primary .fixed-header-table th {
+          background-color: #c4cfd9; /* #afceea */
+      }
+
+      .section.primary.tab header, .section.primary.tab .block header, .section.primary.tab .block, .section.primary .block {
+          background-color: #dae6f2;
+      }
+
+      .section.secondary.tab header, .section.secondary.tab .block header, .section.secondary.tab .block, .section.secondary .block {
+          background-color: #dae6f2;
+      }
+
+      .section.tertiary.tab header, .section.tertiary.tab .block header, .section.tertiary.tab .block, .section.tertiary .block {
+          background-color: #dae6f2;
+      }
+
+      .section.quaternary.tab header, .section.quaternary.tab .block header, .section.quaternary.tab .block, .section.quaternary .block {
+          background-color: #dae6f2;
+      }
+
+      .section.quinary.tab header, .section.quinary.tab .block header, .section.quinary.tab .block, .section.quinary .block {
+          background-color: #dae6f2;
+      }
+
+      .section.senary.tab header, .section.senary.tab .block header, .section.senary.tab .block, .section.senary .block {
+          background-color: #dae6f2;
+      }
+
+      .section.septenary.tab header, .section.septenary.tab .block header, .section.septenary.tab .block, .section.septenary .block {
+          background-color: #dae6f2;
+      }
+
+      .section.octonary.tab header, .section.octonary.tab .block header, .section.octonary.tab .block, .section.octonary .block {
+          background-color: #dae6f2;
       }`;
 
       var canvasBulkMessageCSSCode = `
@@ -235,40 +278,40 @@
             if (rc_summary == 'rc_summary.php') {
 
               var byStudentLink = `<a class="icon rce" href="https://www.palcschool.org/moodle/palcs20/dashboard/teacher/rc_entry_by_student.php?courseid=`+pCourseID+`">By Student</a>`;
-      
+
               var pCaption = document.getElementsByTagName('caption')[0];
-      
+
               pCaption.lastElementChild.lastElementChild.lastElementChild.innerHTML += byStudentLink;
-      
+
           } else if (rc_entry == 'rc_entry.php') {
 
               if (document.getElementsByClassName('infomsg')[0]) {
-      
+
                 var gradingInfoMsg = document.getElementsByClassName('infomsg')[0].innerText;
                 var infomsg = 'Grading not open for course.';
                 //console.log('We\'re at RC Entry, By Period tab');
-        
+
                 if (gradingInfoMsg == infomsg && infomsg != 'undefined'){
-        
+
                 var byStudentLink2 = `<a class="icon rce" style="text-align:right;float:right;" href="https://www.palcschool.org/moodle/palcs20/dashboard/teacher/rc_entry_by_student.php?courseid=`+pCourseID+`">By Student</a>`;
-        
+
                 var gradingInfoMsg2 = document.getElementsByClassName('infomsg')[0];
-        
+
                 gradingInfoMsg2.innerHTML += byStudentLink2;
-        
+
                 }
               } else if (document.getElementsByClassName('infomsg')[0] == null && pCourseID) {
 
                 var byStudentLink3 = `<a class="icon rce" href="https://www.palcschool.org/moodle/palcs20/dashboard/teacher/rc_entry_by_student.php?courseid=`+pCourseID+`">By Student</a>`;
 
                 //console.log('Active and true');
-      
+
                 var pCaption3 = document.getElementsByTagName('caption')[0];
 
                 pCaption3.lastElementChild.lastElementChild.lastElementChild.innerHTML += byStudentLink3;
 
               }
-      
+
           }
         }
       }
@@ -290,6 +333,12 @@
           addPalcschoolUiStyle(dashboardWidthCSSCode);
         }
 
+        if (typeof config.dashboardColorsCSS !== 'undefined' && !config.dashboardColorsCSS) {
+          return;
+        } else {
+          addPalcschoolUiStyle(dashboardColorsCSSCode);
+        }
+
         if (typeof config.canvasBulkMessageCSS !== 'undefined' && !config.canvasBulkMessageCSS) {
           return;
         } else {
@@ -302,10 +351,10 @@
           addPalcschoolUiStyle(keyframesHolderCSSCode);
         }
 
-        if (typeof config.addPwrSchoolNavigation !== 'undefined' && !config.addPwrSchoolNavigation) {
+        if (typeof config.addIEPWriterNavigation !== 'undefined' && !config.addIEPWriterNavigation) {
           return;
         } else {
-          pwrSchoolNav();
+          iepWriterNav();
         }
 
 
